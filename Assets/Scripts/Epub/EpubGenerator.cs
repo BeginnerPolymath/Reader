f276460f0b470cb2685e78cb5d2f2c01c9228c4b
BeginnerPolymath <pavelkotovk@gmail.com>
2020-06-11 04:06:43 +1000

    Epub

---
+++ Assets/Scripts/Epub/EpubGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EpubSharp;
+
+public class EpubGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EpubBook book = EpubReader.Read("my.epub");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
