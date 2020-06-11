8e53fc36494083bf42e20836d193f449f852a030
BeginnerPolymath <pavelkotovk@gmail.com>
2020-06-12 03:25:10 +1000

    Commet

--- Assets/Scripts/Epub/EpubGenerator.cs
+++ Assets/Scripts/Epub/EpubGenerator.cs
@@ -4,32 +4,61 @@ using UnityEngine;
 using System.IO.Compression;
 using System.Xml;
 using System.IO;
-using EpubSharp.Format;
+using EpubSharp;
 
 public class EpubGenerator : MonoBehaviour
 {
-    private ZipArchive Open(Stream stream, bool leaveOpen)
+    public int BookID;
+
+    public List<XmlDocument> Chapters = new List<XmlDocument>();
+
+    public List<string> ChaptersString = new List<string>();
+
+    ZipArchive EpubFile;
+
+    public List<FileStream> Books = new List<FileStream>();
+    
+    public List<string> PathBooks = new List<string>()
     {
-        return new ZipArchive(stream, ZipArchiveMode.Read, leaveOpen, Constants.DefaultEncoding);
-    }
+        "Assets/Books/Epub/igp-twss.epub",
+        "Assets/Books/Epub/Fresko_Vse-luchshee-chto-ne-kupish-za-dengi.375942.fb2.epub",
+    };
 
     // Start is called before the first frame update
     void Start()
     {
-        FileStream x = new FileStream("Assets/Books/Epub/Fresko_Vse-luchshee-chto-ne-kupish-za-dengi.375942.fb2.epub", FileMode.Open, FileAccess.Read);
+        EpubBook book = EpubReader.Read(PathBooks[BookID]);
 
-        ZipArchive z = new ZipArchive(x);
+        ICollection<EpubChapter> chapters = book.TableOfContents;
+
+        print(book.Title);
+
+        Books.Add(new FileStream("Assets/Books/Epub/igp-twss.epub", FileMode.Open, FileAccess.Read));
+        Books.Add(new FileStream("Assets/Books/Epub/Fresko_Vse-luchshee-chto-ne-kupish-za-dengi.375942.fb2.epub", FileMode.Open, FileAccess.Read));
+
+        EpubFile = new ZipArchive(Books[BookID]);
+
+        foreach (var item in chapters)
+        {
+            Chapters.Add(new XmlDocument());
+            Chapters[Chapters.Count-1].Load(EpubFile.GetEntry(item.AbsolutePath.Remove(0, 1)).Open());
+
+            foreach (var itemz in item.SubChapters)
+            {
+                Chapters.Add(new XmlDocument());
+                Chapters[Chapters.Count-1].Load(EpubFile.GetEntry(itemz.AbsolutePath.Remove(0, 1)).Open());
+            }
+        }
+
+
+        //EpubFile.GetEntry($"OPS/ch{chID}.xhtml");
 
         XmlDocument bookFile = new XmlDocument();
 
-        foreach (ZipArchiveEntry item in z.Entries)
+        foreach (XmlDocument item in Chapters)
         {
-            print(item.FullName);
+            //print(item.InnerXml);
         }
-
-        bookFile.Load(z.GetEntry("OPS/ch1.xhtml").Open());
-
-        print(bookFile.InnerText);
     }
 
     // Update is called once per frames
